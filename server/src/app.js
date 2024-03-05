1e52af254c0cef990442ae607842b918e8455a30
huzaifaghazali <huzaifaghazali@gmail.com>
2024-03-05 15:34:52 +0500

    Add cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 
 const api = require('./routes/api');
 
+app.use(cors({
+   origin: 'http://localhost:3000'
+}));
 app.use(express.json());
 app.use('/v1', api);
 
