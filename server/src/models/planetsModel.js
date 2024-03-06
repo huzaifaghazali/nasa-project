f094e4976e68e71515e124c7f6855b1945ed4499
huzaifaghazali <huzaifaghazali@gmail.com>
2024-03-06 15:06:07 +0500

    Add planets model and data

--- server/src/models/planetsModel.js
+++ server/src/models/planetsModel.js
@@ -1,3 +1,43 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+}
+
+// The pipe function is meant to connect a readable stream source to a writable stream destination, a stream that takes in data as opposed to giving you data. So the Kepler file is our source, and the parse function is the destination for our pipe.
+
+fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
+  .on('data', (data) => {
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', (err) => {
+    console.log(err);
+  })
+  .on('end', () => {
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet['kepler_name'];
+      })
+    );
+    console.log(`${habitablePlanets.length} habitable planets found`);
+  });
+
+module.exports = {
+  planet: habitablePlanets,
+};
