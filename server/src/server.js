a58691b7711a1c0a44a9ce0c640a8009804b6ad2
huzaifaghazali <huzaifaghazali@gmail.com>
2024-03-06 16:05:13 +0500

    Load planets data

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,15 @@
 const http = require('http');
 
 const app = require('./app');
+const { loadPlanetsData } = require('./models/planetsModel');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-function startServer() {
+async function startServer() {
+  await loadPlanetsData();
+
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
