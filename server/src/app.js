4967f731a7676c1d40fa4b53b0b37f634011ab34
huzaifaghazali <huzaifaghazali@gmail.com>
2024-03-11 16:55:49 +0500

    Add morgan middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -10,6 +11,7 @@ const api = require('./routes/api');
 app.use(cors({
    origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/v1', api);
