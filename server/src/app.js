486a9b2051e7156614d1f7f3d91d92387765a436
huzaifaghazali <huzaifaghazali@gmail.com>
2024-03-11 13:12:30 +0500

    Server front end code in production

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -10,6 +11,11 @@ app.use(cors({
    origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use('/v1', api);
+
+app.get('/*', (req, res) => {
+   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+ });
 
 module.exports = app;
