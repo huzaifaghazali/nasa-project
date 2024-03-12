ef0dab208b6fab104cc4273750ba50d8804f8096
huzaifaghazali <huzaifaghazali@gmail.com>
2024-03-12 15:49:16 +0500

    Add delete launch functionality

--- server/src/routes/launches/launchesController.js
+++ server/src/routes/launches/launchesController.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require('../../models/launchesModel');
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById
+} = require('../../models/launchesModel');
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -30,7 +35,23 @@ function httpAddNewLaunch(req, res) {
   return res.status(201).json(launch);
 }
 
+function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+
+  // if launch doesn't exist
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: 'Launch not found',
+    });
+  }
+
+  // if launch does exist
+  const aborted = abortLaunchById(launchId)
+  return res.status(200).json(aborted);
+}
+
 module.exports = {
   httpGetAllLaunches,
   httpAddNewLaunch,
+  httpAbortLaunch,
 };
