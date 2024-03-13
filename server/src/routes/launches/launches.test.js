e0f60f96767e26daffd2946d5227df23e7f1f687
huzaifaghazali <huzaifaghazali@gmail.com>
2024-03-13 16:05:26 +0500

    Add post launches request test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -7,13 +7,43 @@ describe('Test GET /launches', () => {
       .get('/v1/launches')
       .expect('Content-Type', /json/)
       .expect(200);
-    
   });
 });
 
-
 describe('Test POST /launch', () => {
-  test('It should respond with 200 success', () => {});
+  const completeLaunchData = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-62 f',
+    launchDate: 'January 4, 2028',
+  };
+
+  const launchDataWithoutDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-62 f',
+  };
+
+  const launchDataWithInvalidDate = {
+    mission: 'USS Enterprise',
+    rocket: 'NCC 1701-D',
+    target: 'Kepler-62 f',
+    launchDate: 'zoot',
+  };
+
+  test('It should respond with 201 created', async () => {
+    const response = await request(app)
+      .post('/v1/launches')
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
 
   test('It should catch missing required properties', () => {});
 
