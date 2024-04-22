a2e8441140241197cec1a6b4ebbd33a48b4a36c4
huzaifaghazali <huzaifaghazali@gmail.com>
2024-04-22 20:46:29 +0500

    Add mongoDB connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,9 @@
 const http = require('http');
 
+require('dotenv').config();
+
 const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planetsModel');
 
 const PORT = process.env.PORT || 8000;
@@ -8,6 +11,7 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 async function startServer() {
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
